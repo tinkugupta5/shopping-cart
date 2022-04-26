7d4bdb840d72870d33c9e6df3e767cdae31e4a15
tinku gupta <tinku.gupta4@gmail.com>
2022-04-26 23:46:26 +0530

    first hero section completed  commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+  } from "react-router-dom";
+import Home from './pages/Home';
+// import About from './pages/About';
+import Navigation from "./components/Navigation";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const  App  = () =>
+{
+    return (
+        <>
+
+       <Router>
+       <Navigation/>
+            <Routes>
+        
+                <Route path="/" element={<Home/>}></Route>
+                {/* <Route path="/about" element={<About/>}></Route> */}
+                <Route path="/products" element={<Products/>}></Route>
+                <Route path="/cart" element={<Cart/>}></Route>
+
+            </Routes>
+        </Router> 
+       
+    </>
+    )
 }
 
+
+
 export default App;
+
+
---
+++ src/pages/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <h1>Hellow from About Page</h1>
+  )
+}
+
+export default About
