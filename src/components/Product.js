3c3f4a72ce623920201b6ca13a984d93472c2758
tinku gupta <tinku.gupta4@gmail.com>
2022-05-22 18:58:54 +0530

    add green color to button

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,11 @@
-import React from 'react'
+// import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { CartContext } from '../CartContext';
 
   const Product = (props) => {
+    // added green button for changing the state 
+    const [isAdding, setIsAdding] = useState(false);
   const {cart , setCart} = useContext(CartContext);
   // console.log(props);
   const {product} = props;
@@ -32,6 +34,11 @@ import { CartContext } from '../CartContext';
 
     _cart.totalItems +=1; //or _cart.totalItems = _cart.totalItem + 1;
     setCart(_cart); 
+    setIsAdding(true);
+
+    setTimeout(() => {
+      setIsAdding(false);
+    },1000)
 
    
 
@@ -65,7 +72,7 @@ import { CartContext } from '../CartContext';
     
     <div className='flex justify-between items-center mt-4'>
         <span>₹ {product.price}</span>
-        <button onClick={(e) => {addToCart(e,product)}} className='bg-yellow-500 py-1 px-4 rounded-full font-bold'>ADD</button>
+        <button onClick={(e) => {addToCart(e,product)}} className={`${isAdding ? 'bg-green-500' : 'bg-yellow-500' } py-1 px-4 rounded-full font-bold`}>ADD{isAdding ? 'ED' : ''}</button>
     </div>
 
     </div>
