3d30be51631d270261da070025b2677192583a69
tinku gupta <tinku.gupta4@gmail.com>
2022-06-19 14:29:07 +0530

    this is an cart page changes

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -5,6 +5,15 @@ const Cart = () => {
 
      <div className='container mx-auto lg:w-1/2 w-full pb-24'>
       <h1 className='my-12 font-bold'>Cart items</h1>
+      <ul>
+        <li>
+          <div> 
+            <div>
+              <h1>This is an example</h1>
+            </div>
+          </div>
+        </li>
+      </ul>
 
      </div>
 
