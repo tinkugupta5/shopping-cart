5abc0bef14a2b834ed15c1e91717fa1b1b4c43bc
tinku gupta <tinku.gupta4@gmail.com>
2022-06-20 23:40:19 +0530

    working on cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,19 +3,56 @@ import React from 'react'
 const Cart = () => {
   return (
 
-     <div className='container mx-auto lg:w-1/2 w-full pb-24'>
+    <div className='container mx-auto lg:w-1/2 w-full pb-24'>
       <h1 className='my-12 font-bold'>Cart items</h1>
       <ul>
-        <li>
-          <div> 
-            <div>
-              <h1>This is an example</h1>
+        <li className='mb-12 '>
+          <div className='flex items-center justify-between'>
+            <div className='flex item-center'>
+              <img className='h-16' src='/images/peproni.png' alt='' />
+              <span className='font-bold ml-4 w-48'>Double Peperoni</span>
             </div>
+            {/* increment and decrement */}
+            <div>
+              <button className='bg-yellow-500 px-4 py-2 rounded-full leading-none'  >-</button>
+              <b className='px-2'>2</b>
+              <button className="bg-yellow-500 px-4 py-2 rounded-full leading-none">+</button>
+            </div>
+            <span> $ 500 </span>
+            <button className='bg-red-500 px-4 py-2 rounded-full leading-none text-white'>Delete</button>
+
           </div>
         </li>
+        <li className='mb-12 '>
+          <div className='flex items-center justify-between'>
+            <div className='flex item-center'>
+              <img className='h-16' src='/images/peproni.png' alt='' />
+              <span className='font-bold ml-4 w-48'>Double Peperoni</span>
+            </div>
+            {/* increment and decrement */}
+            <div>
+              <button className='bg-yellow-500 px-4 py-2 rounded-full leading-none'  >-</button>
+              <b className='px-2'>2</b>
+              <button className="bg-yellow-500 px-4 py-2 rounded-full leading-none">+</button>
+            </div>
+            <span> $ 500 </span>
+            <button className='bg-red-500 px-4 py-2 rounded-full leading-none text-white'>Delete</button>
+
+          </div>
+        </li>
+
+        <hr className='my-6' />
+
+        <div className='text-right'>
+          <b>Grand Total:</b>   ₹ 1000
+        </div>
+        <div class="text-right mt-6">
+          <button className='bg-yellow-500 px-4 py-2 rounded-full leading-none'>Order Now</button>
+        </div>
+
       </ul>
 
-     </div>
+    </div>
 
   )
 }
